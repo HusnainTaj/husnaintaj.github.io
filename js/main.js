74eab645fd95d6f5b7ab6aa27d41fabd478a038a
Husnain Taj <85726252+HusnainTaj@users.noreply.github.com>
2023-06-10 15:09:51 +0500

    js update

--- js/main.js
+++ js/main.js
@@ -1,100 +1,11 @@
 "use strict";
 
 let ks = new KonsoleSettings();
-ks.animatePrint = false;
-ks.printLetterInterval = 20;
+ks.animatePrint = true;
+ks.printLetterInterval = 10;
 ks.registerDefaultKommands = false;
 ks.prefix = "T:/>";
 let konsole = new Konsole("#Console", ks);
-
-let data = {
-    "about":[
-        "I'm Husnain Taj.", 
-        `A Programmer...`,
-        `Try some other commands.`
-    ],
-    "languages":[
-        "C#",
-        "Javascript + Typescript",
-        "Python",
-        "Java + Kotlin",
-        "C++",
-        "Rust [planning]",
-        "Dart [planning]"
-    ],
-    "projects":[
-        {
-            name:"KonsoleJS",
-            desc:"A JS Library for building console like UI in your website.",
-            url:"https://github.com/HusnainTaj/KonsoleJS"
-        },
-        {
-            name:"DPI Finder",
-            desc:"Simple web app to find DPI of a mouse.",
-            url:"https://github.com/HusnainTaj/DPI-Finder"
-        },
-        {
-            name:"Draggable Nestable List",
-            desc:"JavaScript Library for adding rearranging and nesting functionality to simple lists.",
-            url:"https://github.com/HusnainTaj/DraggableNestableList"
-        }
-    ],
-    "technologies":[
-        {
-            "name":"Web",
-            "items":[
-                `Blazor`,   
-                `ASP.NET MVC`,
-                `Razor Pages`,
-                `Angular`,
-                `Firebase`,
-                `Wordpress`,
-            ]
-        },
-        {
-            "name":"Desktop",
-            "items":[
-                `Electron`,
-                `Windows Forms`,
-                `Windows Presentation Foundation (WPF)`,
-                `Windows Services`,
-                `Tauri [planning]`,
-            ]
-        },
-        {
-            "name":"Android",
-            "items":[
-                "Native",
-            ]
-        },
-        {
-            "name":"Cross Platform",
-            "items":[
-                `MAUI [planning]`,
-                `Flutter [planning]`,
-            ]
-        },
-        {
-            "name":"Testing",
-            "items":[
-                "NUnit",
-                "Stryker Mutator [planning]",
-            ]
-        },
-        {
-            "name":"Crypto",
-            "items":[
-                "Solidity",
-                "Solana Smart Contracts using Rust [planning]"
-            ]
-        }
-    ],
-    "links":[
-        {name:"Email", url:"mailto:husnain.taj@gmail.com"},
-        {name:"GitHub", url:"https://github.com/HusnainTaj/"},
-        {name:"LinkedIn", url:"https://www.linkedin.com/in/husnain-taj/"},
-    ]
-};
 
 function toAnchorTag(text, url)
 {
@@ -122,42 +33,38 @@ $(async ()=>{
 
     konsole.RegisterKommand(new Kommand("about", "me", null, ()=>{
         return new Promise((resolve, reject)=>{
-            konsole.print(...data.about).then(resolve);
+            konsole.print(...medata.about).then(resolve);
         });
     }));
 
-    // konsole.RegisterKommand(new Kommand("quiz", "test your programming IQ.", null, ()=>{
-    //     return new Promise(async (resolve, reject)=>{
-    //         let correctAnsCount = 0;
-    //         let totalQuesCount = 2;
+    konsole.RegisterKommand(new Kommand("quiz", "test your programming IQ.", null, ()=>{
+        return new Promise(async (resolve, reject)=>{
+            let correctAnsCount = 0;
+            let totalQuesCount = 2;
 
-    //         // await konsole.print([
-    //         //     "before you start",
-    //         //     "i want you to know that even if you fail the quiz",
-    //         //     "do not let it dishearten you",
-    //         //     "instead use it as a guide to discover what you can learn next.",
-    //         //     "good luck!"
-    //         // ]);
-
-    //         if(await konsole.mcq("Q.1) which programming language is the best?", [ "Java", "C++", "Python", "Assembly"], "C#", "score++;","score--; correct answer is C#.")) correctAnsCount++;
+            await konsole.print("Use arrow keys to select your answer");
+            await konsole.print("good luck!");
+            await konsole.print("----------");
             
-    //         if(await konsole.mcq("Q.2) was this quiz fun?", ["Yes", "Yes", "Yes", "No"], "Yes", "score++;","score--;")) correctAnsCount++;
+            if(await konsole.mcq("Q.1) which programming language is the best?", [ "Java", "C++", "Python", "Assembly"], "C#", "score++;\n","score--; correct answer is C#.\n")) correctAnsCount++;
+            
+            if(await konsole.mcq("Q.2) was this quiz fun?", ["Yes", "Yes", "Yes", "No"], "Yes", "score++;","score--;")) correctAnsCount++;
 
-    //         await konsole.print(`score: ${correctAnsCount}/${totalQuesCount}`)
+            await konsole.print(`score: ${correctAnsCount}/${totalQuesCount}`)
 
-    //         konsole.awaitKommand();
-    //     });
-    // }));
+            konsole.awaitKommand();
+        });
+    }));
 
     konsole.RegisterKommand(new Kommand("langs", "programming languages i've worked with.", null, ()=>{
         return new Promise((resolve, reject)=>{
-            konsole.print(...data.languages).then(resolve);
+            konsole.print(...medata.languages).then(resolve);
         });
     }));
 
     konsole.RegisterKommand(new Kommand("projects", "projects i've worked or working on.", null, ()=>{
         return new Promise(async (resolve, reject)=>{
-            for (const project of data.projects) {
+            for (const project of medata.projects) {
                 await konsole.print(`${toAnchorTag(project.name, project.url)} - ${project.desc}`);
             }
             resolve();
@@ -166,7 +73,7 @@ $(async ()=>{
 
     konsole.RegisterKommand(new Kommand("tech", "frameworks and libraries i've worked with or interested in learning.", null, ()=>{
         return new Promise(async (resolve, reject)=>{
-            for (const tech of data.technologies) {
+            for (const tech of medata.technologies) {
                 await konsole.print(`${tech.name}\n${"¯".repeat(tech.name.length)}\n    ${tech.items.join("\n    ")}`);
             }
             resolve();
@@ -175,7 +82,7 @@ $(async ()=>{
 
     konsole.RegisterKommand(new Kommand("links", "links to my socials...", null, ()=>{
         return new Promise(async (resolve, reject)=>{
-            for (const link of data.links) {
+            for (const link of medata.links) {
                 await konsole.print(toAnchorTag(link.name, link.url));
             }
             resolve();
---
+++ me.js
@@ -0,0 +1,100 @@
+class category
+{
+    name = "";
+    items = [];
+
+    constructor(n,i)
+    {
+        this.name = n;
+        this.items = i;
+    }
+};
+
+class project
+{
+    name = "";
+    desc = "";
+    url = "";
+
+    constructor(n,d,u)
+    {
+        this.name = n;
+        this.desc = d;
+        this.url = u;
+    }
+};
+
+
+class link
+{
+    name = "";
+    url = "";
+
+    constructor(n,d,u)
+    {
+        this.name = n;
+        this.url = u;
+    }
+};
+
+let medata = {
+    "about":[
+        "I'm Husnain Taj.", 
+        `A Programmer...`,
+        `Try some other commands.`
+    ],
+    "languages":[
+        "C#",
+        "Javascript + Typescript",
+        "C++",
+        "Python",
+        "Java + Kotlin",
+        "Rust [planning]",
+        "Dart [planning]"
+    ],
+    "projects":[
+        new project("KonsoleJS", "A JS Library for building console like UI in your website.", "https://github.com/HusnainTaj/KonsoleJS"),
+        new project("DPI Finder", "Simple web app to find DPI of a mouse.", "https://github.com/HusnainTaj/DPI-Finder"),
+        new project("Draggable Nestable List", "JavaScript Library for adding rearranging and nesting functionality to simple lists.", "https://github.com/HusnainTaj/DraggableNestableList"),
+    ],
+    "technologies":[
+        new category("Web",
+        [
+            `Blazor`,   
+            `ASP.NET MVC`,
+            `Razor Pages`,
+            `Angular`,
+            `Firebase`,
+            `Wordpress`,
+        ]),
+        new category("Desktop",
+        [
+            `Electron`,
+            `Windows Forms`,
+            `Windows Presentation Foundation (WPF)`,
+            `Windows Services`,
+            `Tauri`,
+        ]),
+        new category("Android",
+        [
+            "Native",
+            `MAUI [planning]`,
+            `Flutter [planning]`,
+        ]),
+        new category("Testing",
+        [
+            "NUnit",
+            "Stryker Mutator [planning]",
+        ]),
+        new category("Crypto/Web3",
+        [
+            "Solidity",
+            "Solana Smart Contracts using Rust [planning]"
+        ]),
+    ],
+    "links":[
+        new link("Email","mailto:husnain.taj@gmail.com" ),
+        new link("GitHub","https://github.com/HusnainTaj/"),
+        new link("LinkedIn","https://www.linkedin.com/in/husnain-taj/"),
+    ]
+};
